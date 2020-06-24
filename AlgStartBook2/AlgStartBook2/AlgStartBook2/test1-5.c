f718e163db9512dc33921bd7fbafd7849a8094f7
wen-fei <282699766@qq.com>
2020-06-24 23:08:11 +0800

    1-5

---
+++ AlgStartBook2/AlgStartBook2/AlgStartBook2/test1-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+	const double pi = acos(-1.0);
+	double r, h, s1, s2, s;
+	scanf_s("%lf%lf", &r, &h);
+	s1 = pi * r * r;
+	s2 = 2 * pi * r * h;
+	s = s1 * 2.0 + s2;
+	printf("Area = %3.f\n", s);
+	return 0;
+}
