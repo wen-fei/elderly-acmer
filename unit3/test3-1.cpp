2880be4cb8641c61cd7c77cd0b1201e6a7a9919e
wen-fei <282699766@qq.com>
2020-06-28 23:40:48 +0800

    test3-1

---
+++ unit3/test3-1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#define maxn 105
+int a[maxn];
+
+int main() {
+    int x, n =0;
+    while(scanf("%d", &x) == 1) {
+        a[n++] = x;
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        printf("%d ", a[i]);
+    }
+
+    printf("%d\n", a[0]);
+    system("pause");
+    return 0;
+}
